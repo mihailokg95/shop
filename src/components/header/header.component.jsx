23b1242dddc31b1c8b4d9e739bf9352680717146
mihailokg95 <mstojkovic955@gmail.com>
2021-02-26 21:04:22 +0100

    Logo and some styling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import './header.styles.scss';
-import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/4.3 crown.svg';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import "./header.styles.scss";
 
 const Header = () => (
-  <div className='header'>
-    <Link to='/' className='logo-container'>
-        <Logo className='logo'/>
+  <div className="header">
+    <Link to="/" className="logo-container">
+      <Logo className="logo" />
     </Link>
-      <div className='options'>
-        <Link className ='option' to='/shop'>SHOP</Link>
-        <Link className ='option' to='/shop'>CONTACT</Link>
-      </div>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
+    </div>
   </div>
 );
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,9 @@
 import "./style.scss";
-import logo from "../../assets/logo.svg";
 import Directory from "../../components/directory/directory.component";
 
 function Homepage() {
   return (
     <div className="homepage">
-      <img src={logo} alt="logo" />
       <Directory />
     </div>
   );
